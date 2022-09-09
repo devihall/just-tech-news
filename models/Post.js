959657747c8db64b337714a9239870bbbe4df4d5
devihall <devis0306@gmail.com>
2022-09-09 13:21:27 -0400

    define model associations

--- models/Post.js
+++ models/Post.js
@@ -4,39 +4,41 @@ const sequelize = require("../config/connection");
 // create our Post model
 class Post extends Model {}
 
+// create fields/columns for Post model
 Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      autoIncrement: true
     },
     title: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: false
     },
     post_url: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isURL: true,
-      },
+        isURL: true
+      }
     },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "user",
-        key: "id",
-      },
-    },
+        model: 'user',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: "post",
+    modelName: 'post'
   }
 );
+
 
 module.exports = Post;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-// GET /api/users
+// get all users
 router.get("/", (req, res) => {
-  // Access our User model and run .findAll() method)
   User.findAll({
     attributes: { exclude: ["password"] },
   })
@@ -14,7 +13,6 @@ router.get("/", (req, res) => {
     });
 });
 
-// GET /api/users/1
 router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
@@ -35,7 +33,6 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// POST /api/users
 router.post("/", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -63,6 +60,7 @@ router.post("/login", (req, res) => {
     }
 
     const validPassword = dbUserData.checkPassword(req.body.password);
+
     if (!validPassword) {
       res.status(400).json({ message: "Incorrect password!" });
       return;
@@ -72,7 +70,6 @@ router.post("/login", (req, res) => {
   });
 });
 
-// PUT /api/users/1
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
@@ -96,7 +93,6 @@ router.put("/:id", (req, res) => {
     });
 });
 
-// DELETE /api/users/1
 router.delete("/:id", (req, res) => {
   User.destroy({
     where: {
